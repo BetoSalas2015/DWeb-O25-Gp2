e117b91c4e65b085020d00b9f6cd71abc494e906
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2025-08-22 17:37:52 -0600

    Sesion 2: 22/08/2025

---
+++ 01 - javascript/Ejercicio02.js
@@ -0,0 +1,47 @@
+var a = 5.5             //  number
+console.log(a);
+
+a = "Hola"              //  Cadena (string)
+console.log(a);
+
+a = 'a'                 // caracter
+console.log(a);
+
+a = function name(params) {     // funciones
+    
+}
+console.log(a);
+
+a = undefined            // undfined  (sin definir)
+console.log(a);
+
+a = null                // null (nulo)
+console.log(a);
+
+a = []                  //  Listas o arreglos
+console.log(a);
+
+a = {}                  //  Objetos
+console.log(a);
+
+a = true                //  boolean
+console.log(a);
+
+var b
+console.log(b);
+
+console.log( null == undefined );
+console.log( null === undefined );
+
+console.log( 0 == '0');
+console.log( 0 === '0');
+console.log( 48 === '0');
+console.log( 48 == '48');
+
+
+
+   
+
+
+
+
